b57cac160dcff995e5a5343c6b75443be961c886
tomocy <tomocy.dev@gmail.com>
2018-04-24 21:10:25 +0900

    [mod] modify parameters

--- app/src/main/java/com/tomocy/calculator/States/State.java
+++ app/src/main/java/com/tomocy/calculator/States/State.java
@@ -1,17 +1,18 @@
 package com.tomocy.calculator.States;
 
 import com.tomocy.calculator.Context;
+import com.tomocy.calculator.Operation;
 
 public interface State {
 
     void onInputNumber(Context ctx, Number num);
 
-    void onInputOperation(Context ctx);
+    void onInputOperation(Context ctx, Operation op);
 
     void onInputEqual(Context ctx);
 
     void onInputClaer(Context ctx);
 
-    void onInputAllClear();
+    void onInputAllClear(Context ctx);
 
 }
