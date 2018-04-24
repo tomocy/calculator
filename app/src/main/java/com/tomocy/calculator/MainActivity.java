e6a9ad113bd71024fc8ddcd2994e2b55b7090358
tomocy <tomocy.dev@gmail.com>
2018-04-24 20:20:16 +0900

    first commit

---
+++ app/src/main/java/com/tomocy/calculator/MainActivity.java
@@ -0,0 +1,17 @@
+package com.tomocy.calculator;
+
+/**
+ * Simple calculator android application
+ * Referenced from http://codezine.jp/
+ */
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
