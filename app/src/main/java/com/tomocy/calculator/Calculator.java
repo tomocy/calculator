af692bb3ca67914d2f30ca9af44a3b55efaff38f
tomocy <tomocy.dev@gmail.com>
2018-04-26 07:40:24 +0900

    [mod] reimplement Context

--- app/src/main/java/com/tomocy/calculator/Calculator.java
+++ app/src/main/java/com/tomocy/calculator/Calculator.java
@@ -1,5 +1,9 @@
 package com.tomocy.calculator;
 
+import android.widget.TextView;
+
+import com.tomocy.calculator.Displays.Display;
+import com.tomocy.calculator.Displays.TextViewDisplay;
 import com.tomocy.calculator.States.State;
 import com.tomocy.calculator.States.NumberAState;
 
@@ -13,30 +17,23 @@ public class Calculator implements Context {
 
     private State state;
 
+    private Display disp;
+
     public Calculator() {
         this.a = 0;
         this.b = 0;
         this.op = null;
         this.state = NumberAState.getInstance();
+        this.disp = null;
     }
 
     @Override
-    public void clear() {
-
+    public void setDisp(Display disp) {
+        this.disp = disp;
     }
 
     @Override
     public void showNum(Number num) {
-
-    }
-
-    @Override
-    public void saveNum() {
-
-    }
-
-    @Override
-    public void setOp(Operation op) {
-
+        this.disp.show(num.toString());
     }
 }
