a39321af9b3fd1bd345a36a34edd2fce57febc7c
tomocy <tomocy.dev@gmail.com>
2018-04-25 15:06:53 +0900

    [wip] a method called when btn is clicked

--- app/src/main/java/com/tomocy/calculator/MainActivity.java
+++ app/src/main/java/com/tomocy/calculator/MainActivity.java
@@ -7,11 +7,22 @@ package com.tomocy.calculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClickButton(View view) {
+        switch (view.getId()) {
+            case R.id.num7Btn:
+                TextView textView = findViewById(R.id.dispTextView);
+                String txt = "aiueo";
+                textView.setText(txt);
+        }
     }
 }
