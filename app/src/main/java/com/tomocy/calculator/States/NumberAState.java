a26bdd6b488a8e08e64ba1da76d255a1afd6f2b3
tomocy <tomocy.dev@gmail.com>
2018-04-26 07:44:08 +0900

    [mod] adjust Context

--- app/src/main/java/com/tomocy/calculator/States/NumberAState.java
+++ app/src/main/java/com/tomocy/calculator/States/NumberAState.java
@@ -1,6 +1,7 @@
 package com.tomocy.calculator.States;
 
 import com.tomocy.calculator.Context;
+import com.tomocy.calculator.Number;
 import com.tomocy.calculator.Operation;
 
 public class NumberAState implements State {
@@ -17,28 +18,25 @@ public class NumberAState implements State {
 
     @Override
     public void onInputNumber(Context ctx, Number num) {
-        ctx.clear();
         ctx.showNum(num);
     }
 
     @Override
     public void onInputOperation(Context ctx, Operation op) {
-        ctx.saveNum();
-        ctx.setOp(op);
     }
 
     @Override
     public void onInputEqual(Context ctx) {
-        ctx.clear();
+
     }
 
     @Override
     public void onInputClaer(Context ctx) {
-        ctx.clear();
+
     }
 
     @Override
     public void onInputAllClear(Context ctx) {
-        ctx.clear();
+
     }
 }
