4a9c87213b5fc7d058c05c9716e6c7d2a80df90f
tomocy <tomocy.dev@gmail.com>
2018-04-26 18:45:26 +0900

    [add] NubmerBState

---
+++ app/src/main/java/com/tomocy/calculator/States/NumberBState.java
@@ -0,0 +1,25 @@
+package com.tomocy.calculator.States;
+
+import com.tomocy.calculator.Context;
+import com.tomocy.calculator.Number;
+
+public class NumberBState implements State {
+
+    private NumberBState() {}
+
+    public static NumberBState getInstance() { return NumberBStateHolder.INSTANCE; }
+
+    private static class NumberBStateHolder {
+        private static final NumberBState INSTANCE = new NumberBState();
+    }
+
+    @Override
+    public void onInputNumber(Context ctx, Number num) {
+
+    }
+
+    @Override
+    public void onInputOperation(Context ctx) {
+
+    }
+}
