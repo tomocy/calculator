d16e71cc2ef745b77461c297915ca902096717a5
tomocy <tomocy.dev@gmail.com>
2018-04-26 14:22:09 +0900

    [mod] reimplements Display

--- app/src/main/java/com/tomocy/calculator/Displays/TextViewDisplay.java
+++ app/src/main/java/com/tomocy/calculator/Displays/TextViewDisplay.java
@@ -2,7 +2,7 @@ package com.tomocy.calculator.Displays;
 
 import android.widget.TextView;
 
-public class TextViewDisplay implements Display {
+public class TextViewDisplay extends DisplayAbstract implements Display {
 
     private TextView disp;
 
@@ -11,8 +11,34 @@ public class TextViewDisplay implements Display {
     }
 
     @Override
-    public void show(String str) {
+    public void add(String str) {
+        this.stack.add(str);
+    }
+
+    @Override
+    public void show() {
+        String str = this.get();
         this.disp.setText(str);
+    }
+
+    @Override
+    public String get() {
+        StringBuffer strBuf = new StringBuffer();
+        for (String str: this.stack) {
+            strBuf.append(str);
+        }
+
+        return strBuf.toString();
+    }
+
+    @Override
+    public Double getAsDouble() {
+        String str = this.get();
+        try {
+            return Double.parseDouble(str);
+        } catch (Exception e) {
+            return 0.0;
+        }
     }
 
 }
