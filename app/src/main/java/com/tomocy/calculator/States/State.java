2d95da7f1e1c6217ac8b12697e1db918971e6682
tomocy <tomocy.dev@gmail.com>
2018-04-26 17:57:05 +0900

    [add] onInputNumber method

--- app/src/main/java/com/tomocy/calculator/States/State.java
+++ app/src/main/java/com/tomocy/calculator/States/State.java
@@ -1,8 +1,11 @@
 package com.tomocy.calculator.States;
 
 import com.tomocy.calculator.Context;
+import com.tomocy.calculator.Number;
 
 public interface State {
+
+    void onInputNumber(Context ctx, Number num);
 
     void onInputOperation(Context ctx);
 
