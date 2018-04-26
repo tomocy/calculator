b7ebc145abb59a79a0c1fb7ccc2ff598d2d57886
tomocy <tomocy.dev@gmail.com>
2018-04-26 17:46:00 +0900

    [mod] change to static method

--- app/src/main/java/com/tomocy/calculator/States/OperationState.java
+++ app/src/main/java/com/tomocy/calculator/States/OperationState.java
@@ -6,7 +6,7 @@ public class OperationState implements State {
 
     private OperationState() {}
 
-    public OperationState getInstance() { return OperationStateHolder.INSTANCE; }
+    public static OperationState getInstance() { return OperationStateHolder.INSTANCE; }
 
     private static class OperationStateHolder {
         private static final OperationState INSTANCE = new OperationState();
