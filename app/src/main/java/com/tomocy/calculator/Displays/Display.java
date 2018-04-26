a8ef6844ec71c87fd950505e1aa93bb608c0f3a3
tomocy <tomocy.dev@gmail.com>
2018-04-26 18:35:37 +0900

    [mod] methods

--- app/src/main/java/com/tomocy/calculator/Displays/Display.java
+++ app/src/main/java/com/tomocy/calculator/Displays/Display.java
@@ -1,8 +1,10 @@
 package com.tomocy.calculator.Displays;
 
+import com.tomocy.calculator.Number;
+
 public interface Display {
 
-    void add(String str);
+    void add(Number num);
 
     void show();
 
