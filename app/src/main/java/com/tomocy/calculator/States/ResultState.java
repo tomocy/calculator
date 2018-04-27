73aed82a881bd330654040a858a94710b8ba4027
tomocy <tomocy.dev@gmail.com>
2018-04-27 12:25:22 +0900

    [add] ResultState

---
+++ app/src/main/java/com/tomocy/calculator/States/ResultState.java
@@ -0,0 +1,21 @@
+package com.tomocy.calculator.States;
+
+import com.tomocy.calculator.Context;
+import com.tomocy.calculator.Number;
+
+public class ResultState implements State {
+    @Override
+    public void onInputNumber(Context ctx, Number num) {
+
+    }
+
+    @Override
+    public void onInputOperation(Context ctx) {
+
+    }
+
+    @Override
+    public void onInputEqual(Context ctx) {
+
+    }
+}
